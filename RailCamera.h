aa969ff8dad718d10e366a1c07a63f04549f33f4
niida_shuto <k021g1267@g.neec.ac.jp>
2022-06-27 00:35:32 +0900

    a

--- RailCamera.h
+++ RailCamera.h
@@ -1,26 +1,26 @@
 #pragma once
 
-//#include"WorldTransform.h"
-//#include"ViewProjection.h"
-/////<summary>
-/////レールカメラ
-/////</summary>
-//class RailCamera
-//{
-//public:
-//	///<summary>
-//	///初期化
-//	///</summary>
-//	void Initialize(WorldTransform worldTransform,WorldTransform worldTransform);
-//
-//	///<summary>
-//	///更新
-//	///</summary>
-//	void Update();
-//private:
-//	//ワールド変換データ
-//	WorldTransform worldTransform_;
-//	//ビュープロジェクション
-//	ViewProjection viewProjection_;
-//};
+#include"WorldTransform.h"
+#include"ViewProjection.h"
+///<summary>
+///レールカメラ
+///</summary>
+class RailCamera
+{
+public:
+	///<summary>
+	///初期化
+	///</summary>
+	void Initialize(WorldTransform worldTransform);
+
+	///<summary>
+	///更新
+	///</summary>
+	void Update();
+private:
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+};
 
